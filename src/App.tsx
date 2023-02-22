978100f73e54b94d62f39c46a9ddcb09b260be36
Voke Okpeka <vefe44@gmail.com>
2023-02-22 10:36:21 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -156,7 +156,7 @@ function App() {
       url.match(
         /^(?:(https?):\/\/)?(?:(?:www|m)\.)?youtu\.be\/([a-zA-Z0-9_-]+)/
       ) ||
-      url.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/);
+      url.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#&?]*).*/);
 
     if (match && match[2].length === 11) {
       return "https://www.youtube.com/embed/" + match[2] + "?showinfo=0";
